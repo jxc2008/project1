e064ffc9def75c687c92f57781258c5e6a022b5a
jxc2008 <jxc2008@nyu.edu>
2025-01-14 16:07:38 -0600

    renderrr

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,13 @@
-from pymongo import MongoClient
+import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+# Fetch the MongoDB URI from an environment variable
 uri = "mongodb+srv://oyggpt:Oygminecraft!@hi-lo-backend.misq6.mongodb.net/?retryWrites=true&w=majority&appName=Hi-Lo-Backend"
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['quant_trading_game']
+# Initialize MongoDB client
+client = MongoClient(uri)
 
+# Define database and collection
+db = client['quant_trading_game']
 rooms_collection = db['rooms']
